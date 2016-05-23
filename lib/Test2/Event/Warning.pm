66fd879ea95623260188f29c415fed344d3219fd
Dave Rolsky <autarch@urth.org>
2016-05-23 09:41:55 -0500

    Bump version after release

--- lib/Test2/Event/Warning.pm
+++ lib/Test2/Event/Warning.pm
@@ -3,7 +3,7 @@ package Test2::Event::Warning;
 use strict;
 use warnings;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use parent 'Test2::Event';
 
--- lib/Test2/Plugin/NoWarnings.pm
+++ lib/Test2/Plugin/NoWarnings.pm
@@ -3,7 +3,7 @@ package Test2::Plugin::NoWarnings;
 use strict;
 use warnings;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use Test2::API qw( context_do );
 
